818278561a88cbadd04184f423774d9154c524f1
kathi <katharina@tideways-gmbh.com>
2020-06-30 11:09:05 +0200

    change rights of data.db

--- .docker/cmd.sh
+++ .docker/cmd.sh
@@ -2,6 +2,7 @@
 set -e
 php bin/console doctrine:database:create
 php bin/console doctrine:migrations:migrate -n
+chown www-data:www-data /app/var/data.db
 node assets/js/sse-hub.js &
 node assets/js/ws-hub.js &
 JWT_KEY='!ChangeMe!' ADDR='0.0.0.0:3000' ./mercure --cors-allowed-origins="http://localhost:8000" &
